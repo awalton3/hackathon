830179bf5f253854b5f48a0e746aebadd5fb3311
Ana Walton <awalton3@nd.edu>
2019-11-09 15:01:10 -0500

    adding database service

---
+++ ndbuspool/src/app/database.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+
+export class DatabaseService {
+
+  createUser(userId, name) {
+
+  }
+
+  setTrip(userId, arrival, departure) {
+
+  }
+
+}
