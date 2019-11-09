6f318d02684902bf8856657c170cf9cfb2bea7a6
Ana Walton <awalton3@nd.edu>
2019-11-09 10:01:58 -0500

    angular is set up

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    //FormsMaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
