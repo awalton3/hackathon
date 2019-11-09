a33d0d41fc69bd0dfb2d11d15b0db21e1df7e4b7
Ana Walton <awalton3@nd.edu>
2019-11-09 10:36:01 -0500

    error

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule
     //FormsMaterialComponentsModule
   ],
   providers: [],
